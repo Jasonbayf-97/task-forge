341c2eca739c5f613e0a62a70493202b48e1c066
Jasonbayford97 <jasonbayford97@gmail.com>
2025-09-27 20:45:10 +0200

    building out the look and feel of the in progress component

--- project-app/src/app/task-progress/task-progress.ts
+++ project-app/src/app/task-progress/task-progress.ts
@@ -4,8 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-task-progress',
   imports: [],
   templateUrl: './task-progress.html',
-  styleUrl: './task-progress.scss'
+  styleUrl: './task-progress.scss',
 })
 export class TaskProgress {
+  tasks = [
+    {
+      name: 'Walk the dogs',
+      description: 'this is a description for a task',
+      time: '2:00',
+      location: 'Park',
+    },
+    {
+      name: 'Prayer walk',
+      description: 'this is a description for task 2',
+      time: '3:00',
+      location: 'Beach',
+    },
+    {
+      name: 'Grocery shopping',
+      description: 'this is a description for task 3',
+      time: '4:00',
+      location: 'Supermarket',
+    },
+  ];
 
+  constructor() {}
 }
